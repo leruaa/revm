b488b0a248714236e56549e9576d521230e7c60c
refcell <abigger87@gmail.com>
2025-03-18 17:49:27 -0400

    reimburse deposits

--- crates/revm/src/optimism/handler_register.rs
+++ crates/revm/src/optimism/handler_register.rs
@@ -282,8 +282,8 @@ pub fn reimburse_caller<SPEC: Spec, EXT, DB: Database>(
     mainnet::reimburse_caller::<SPEC, EXT, DB>(context, gas)?;
 
     // If the transaction isn't a deposit transaction, refund the caller for the operator fee.
-    let is_deposit = context.evm.inner.env.tx.optimism.source_hash.is_some();
-    if !is_deposit && SPEC::SPEC_ID.is_enabled_in(SpecId::ISTHMUS) {
+    // let is_deposit = context.evm.inner.env.tx.optimism.source_hash.is_some();
+    if SPEC::SPEC_ID.is_enabled_in(SpecId::ISTHMUS) {
         let operator_fee_refund = context
             .evm
             .inner
